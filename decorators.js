a7682404346a7f4aaa7b33314d4d829c4a26217e
Leo Fabrikant <leofab86@gmail.com>
2017-06-21 20:50:18 -0400

    re-organizing decorators controller, trying to export functions that require the decorator 6

--- decorators.js
+++ decorators.js
@@ -6,22 +6,21 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-exports.setConfig = setConfig;
-const { STContainer } = require('./stateTracker/STContainer');
-const stateTrackerII = require('./stateTracker/stateTrackerHOC');
-
-let config = {
-	stateTracker: false,
-	updateReports: { mount: false, update: false, pass: false, render: false }
+exports.myDecoratorsConfig = {
+    stateTracker: false,
+    updateReports: { mount: false, update: false, pass: false, render: false }
 };
-exports.myDecoratorsConfig = config;
-function setConfig(configObj) {
-	exports.myDecoratorsConfig = _extends({}, config, configObj);
-}
 
-exports.STContainer = STContainer;
+exports.setConfig = function (configObj) {
+    exports.myDecoratorsConfig = _extends({}, config, configObj);
+};
 
-exports.stateTrackerII = stateTrackerII;
+
+
+
+exports.STContainer = require('./stateTracker/STContainer').STContainer;
+
+exports.stateTrackerII = require('./stateTracker/stateTrackerHOC');
 
 
 exports.updateReporterPP = require('./updateReporterHOC').updateReporterPP;
