9d3787e7b41f83b9cea1b94f02d8f367b6b13d00
Leo Fabrikant <leofab86@gmail.com>
2017-06-21 21:02:15 -0400

    re-organizing decorators controller, separating out HOC modules and letting them set their own config 1

--- decorators.js
+++ decorators.js
@@ -7,20 +7,36 @@ Object.defineProperty(exports, "__esModule", {
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 exports.myDecoratorsConfig = {
-    stateTracker: false,
+    //stateTracker: false,
     updateReports: { mount: false, update: false, pass: false, render: false }
 };
 
-exports.setConfig = function (configObj) {
+exports.setConfig = function setConfig (configObj) {
     exports.myDecoratorsConfig = _extends({}, exports.myDecoratorsConfig, configObj);
 };
 
 
 
+// ---------------------------- STATETRACKER -----------------------------------
+// -----------------------------------------------------------------------------
+
+setConfig({
+    showStateTracker: true
+})
 
 exports.STContainer = require('./stateTracker/STContainer').STContainer;
 exports.stateTrackerII = require('./stateTracker/stateTrackerHOC');
 
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+
+
+// ---------------------------- UPDATEREPORTER ---------------------------------
+// ------------------------------------------------------------------------------
 
 exports.updateReporterPP = require('./updateReporterHOC').updateReporterPP;
 exports.updateReporterII = require('./updateReporterHOC').updateReporterII;
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
